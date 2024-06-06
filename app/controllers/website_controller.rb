4f419c5686bf2dc13371fe4575b12249fa4699ad
Reza Husain <84816920+rezahusain@users.noreply.github.com>
2024-06-06 04:09:51 +0000

    Updated

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,8 +1,5 @@
 class WebsiteController < ApplicationController
   def homepage
-    @cpp_url = "https://logowik.com/content/uploads/images/911_c_logo.jpg"
-    @python_url = "https://logowik.com/content/uploads/images/python4089.logowik.com.webp"
-    @ruby_url = "https://logowik.com/content/uploads/images/rails5177.logowik.com.webp"
     
     render({:template=>"pages_templates/homepage"})
   end
