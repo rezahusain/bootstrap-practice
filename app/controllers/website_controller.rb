89f5d1e6ff46e645defd128156544f370d08ca56
rezahusain <rezahusain99@gmail.com>
2024-06-10 12:06:53 -0500

    Updated website

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -3,4 +3,8 @@ class WebsiteController < ApplicationController
     
     render({:template=>"pages_templates/homepage"})
   end
+  def aboutme
+    
+    render({:template=>"pages_templates/aboutme"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get("/", {:controller=>"website", :action=>"homepage"})
+  get("/aboutme", {:controller=>"website", :action=>"aboutme"})
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
